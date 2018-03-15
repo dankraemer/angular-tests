d0c865c551e1f6fb7b25dfe16d450d4c765450b9
Dan Kraemer <dan.kraemer1@gmail.com>
2018-03-15 00:31:10 -0300

    Test TypeScript class construnctor and limitations

---
+++ 1_Intro/TypeScript/ts-constructors/main.ts
@@ -0,0 +1,24 @@
+
+// TypeScript supports only one constructor, unlike C++ constructors with multiple signatures.
+// To surpass this limitation, the fields can be declared as optional
+class Point {
+    x: number;
+    y: number;
+
+    constructor(x?: number, y?: number) {
+        this.x = x;
+        this.y = y;
+    }
+
+    draw() {
+      console.log('X: ' + this.x);
+      console.log('Y: ' + this.y);
+    }
+
+    getDistance() {
+        // ...
+    }
+}
+
+let point = new Point(1, 2);
+point.draw();
