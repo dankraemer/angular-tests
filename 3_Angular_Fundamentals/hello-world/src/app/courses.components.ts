34c7d693d0009f0cb93f39731947821620944966
Dan Kraemer <dan.kraemer1@gmail.com>
2018-03-23 15:41:13 -0300

    Use Angular directivses to render info

--- 3_Angular_Fundamentals/hello-world/src/app/courses.components.ts
+++ 3_Angular_Fundamentals/hello-world/src/app/courses.components.ts
@@ -6,14 +6,29 @@ import { Component } from '@angular/core';
 //    <courses> => "courses"
 //    <div class="courses"> => ".courses"
 //    <div id="courses"> => "#courses"
+// - 'template':
+// {{}} used to render information dynamically
+// This is called String Interpolation
+//   template: '<h2>{{  getTitle() }}</h2>'
+
 @Component({
     selector: 'courses',
-    // {{}} used to render information dynamically
-    // This is called String Interpolation
-    template: '<h2>{{  getTitle() }}</h2>' 
+    // 1. The ` (backtick) will help to break the template into
+    //    multiple lines and make it more readable
+    // 2. Directives are special operation in Angular that
+    //    modifies the structure of the DOM
+    template: `
+        <h2>{{  getTitle() }}</h2>
+        <ul>
+            <li *ngFor="let course of courses">
+                {{ course }}
+            </li>
+        </ul>
+    `
 })
 export class CoursesComponent {
     title = 'List of courses';
+    courses = ["course1", "course2", "course3"];
 
     getTitle() {
         return this.title;
