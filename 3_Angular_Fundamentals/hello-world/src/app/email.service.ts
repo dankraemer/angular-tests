c61e71ca4b7515f06a2b6cf13b0d4e72ace00a50
Dan Kraemer <dan.kraemer1@gmail.com>
2018-03-30 10:10:09 -0300

    Test Angular cli Service creation
    - Use: $ ng create service
    - @Injectable() decorator function explanation

---
+++ 3_Angular_Fundamentals/hello-world/src/app/email.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+
+// @Injectable() is  a decorator function, similar to Component function.
+// It is used when the class has dependencies for its constuctor.
+@Injectable()
+export class EmailService {
+
+  constructor() { }
+
+}
