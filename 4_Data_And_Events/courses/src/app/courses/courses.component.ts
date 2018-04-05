c818be1d4bc5c16eb26fd23420da3e523b0fbdf4
Dan Kraemer <dan.kraemer1@gmail.com>
2018-04-04 22:10:21 -0300

    Test Angular Property binding

--- 4_Data_And_Events/courses/src/app/courses/courses.component.ts
+++ 4_Data_And_Events/courses/src/app/courses/courses.component.ts
@@ -1,5 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+// With property binding, we bind the property of a DOM element (ex. src)
+// with a field or property in our component (ex. imageUrl).
+// 
+// - String Interpolation (preferable for texts in headings, divs, paragraphs):
+//   <h2>{{ title }}</h2>
+//   <img src="{{ imageUrl }}" />
+//
+// - Property binding using brackets '[]' (preferable for images and links): 
+//   <h2 [textContent]="title"> </h2>
+//   <img [src]="imageUrl" />
+//
+// Property binding works only one way, from component to the DOM.
+
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
