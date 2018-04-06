bdebfa97e00a294dc058b90865b412c95a0209fe
Dan Kraemer <dan.kraemer1@gmail.com>
2018-04-05 23:32:41 -0300

    Add Bootstrap into Angular project

--- 4_Data_And_Events/courses/src/app/app.component.ts
+++ 4_Data_And_Events/courses/src/app/app.component.ts
@@ -1,5 +1,13 @@
 import { Component } from '@angular/core';
 
+// $ npm install bootstrap --save
+// Downloads Bootstrap and stores it into node_modules folder.
+// --save flags adds Bootstrap as a dependency in package.json
+
+// The folder node_modules should not be commited into a git repository.
+// Later on package.json can be used to download all project dependencies.
+// $ npm install
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
