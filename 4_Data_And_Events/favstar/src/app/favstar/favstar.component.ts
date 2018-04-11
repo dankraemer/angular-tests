df6e363f2172c9573405c24bd46314f8573079f4
Dan Kraemer <dan.kraemer1@gmail.com>
2018-04-11 20:43:07 -0300

    Exrecise 2: Create component and add Bootstrap

---
+++ 4_Data_And_Events/favstar/src/app/favstar/favstar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-favstar',
+  templateUrl: './favstar.component.html',
+  styleUrls: ['./favstar.component.css']
+})
+export class FavstarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
