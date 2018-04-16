8abe3734c18afd307457010e5c8b0892d2f74cea
Dan Kraemer <dan.kraemer1@gmail.com>
2018-04-16 20:43:51 -0300

    Learn Angular Pige generation

--- 4_Data_And_Events/favstar/src/app/app.module.ts
+++ 4_Data_And_Events/favstar/src/app/app.module.ts
@@ -1,21 +1,25 @@
 import { Angular2FontawesomeModule } from 'angular2-fontawesome/angular2-fontawesome';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { FavstarComponent } from './favstar/favstar.component';
 import { TitlecasingComponent } from './titlecasing/titlecasing.component';
+import { TitleCasePipe } from './title-case.pipe';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     FavstarComponent,
-    TitlecasingComponent
+    TitlecasingComponent,
+    TitleCasePipe
   ],
   imports: [
     BrowserModule,
-    Angular2FontawesomeModule
+    Angular2FontawesomeModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- 4_Data_And_Events/favstar/src/app/titlecasing/titlecasing.component.ts
+++ 4_Data_And_Events/favstar/src/app/titlecasing/titlecasing.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./titlecasing.component.css']
 })
 export class TitlecasingComponent implements OnInit {
+  title: string;
 
   constructor() { }
 
